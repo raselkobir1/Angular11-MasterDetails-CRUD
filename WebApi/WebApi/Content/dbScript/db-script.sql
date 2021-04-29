465b3d005fc8f5419d9301fb0fe153f2e7c1fc4d
raselkobir <raselkobir57@gmail.com>
2021-04-29 22:50:02 +0600

    Signed-off-by: raselkobir <raselkobir57@gmail.com>

---
+++ WebApi/WebApi/Content/dbScript/db-script.sql
@@ -0,0 +1,53 @@
+﻿----------------------------------------- MASTER DETAILS CURD---------------------------------
+CREATE DATABASE RestaurantDB
+GO
+USE RestaurantDB
+GO
+CREATE TABLE Customer(
+CustomerID int primary key identity(1,1),
+Name varchar(50)
+)
+CREATE TABLE Item(
+ItemID int primary key identity(1,1),
+Name varchar(50),
+Price decimal(10,2)
+)
+go
+--MasterTable:
+CREATE TABLE Orders(
+OrderID bigint primary key identity (1,1),
+OrderNo varchar(50),
+CustomerID INT NOT NULL FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+PMethod varchar(50),
+GTotal decimal (18,2)
+)
+go
+--details table:
+create TABLE OrderItems(
+OrderItemID bigint primary key identity(1,1),
+OrderID BIGINT NOT NULL FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+ItemID	   INT NOT NULL FOREIGN KEY (ItemID) REFERENCES Item(ItemID),
+Quantity   INT
+)
+INSERT INTO OrderItems (OrderID,ItemID,Quantity) VALUES(1,2,5)
+---------------drop table Orders
+---------------drop table Customer
+---------------drop table OrderItems
+INSERT INTO Customer (Name) VALUES('Rasel Kabir')
+INSERT INTO Customer (Name) VALUES('Raihan Kabir')
+INSERT INTO Customer (Name) VALUES('Azid khan')
+INSERT INTO Customer (Name) VALUES('Rayed Ahmed')
+go
+select * from Customer
+go
+INSERT INTO Item(Name,Price) VALUES('Chicken Tenders',500)
+INSERT INTO Item(Name,Price) VALUES('Chicken Fries',700)
+INSERT INTO Item(Name,Price) VALUES('Chicken Sandwich',550)
+INSERT INTO Item(Name,Price) VALUES('Grilled Chees Sandwich',400)
+INSERT INTO Item(Name,Price) VALUES('Lettuce and Tomato Burger',300)
+INSERT INTO Item(Name,Price) VALUES('Kashmeri Birayani',450)
+go
+SELECT * from Item
+select * from Customer
+SELECT * from Orders
+SELECT * from OrderItems
---
+++ WebApi/WebApi/Controllers/OrderController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Order
+        public System.Object GetOrders()
+        {
+            var result = (from o in db.Orders
+                          join c in db.Customers on o.CustomerID equals c.CustomerID
+                          select new
+                          {
+                              o.OrderID,
+                              o.OrderNo,
+                              c.Name,
+                              o.PMethod,
+                              o.GTotal
+                          }).ToList();
+            return result;
+        }
+
+        // GET: api/Order/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetOrder(long id)
+        {
+            var order = (from o in db.Orders
+                         where o.OrderID == id
+                         select new
+                         {
+                             o.OrderID,
+                             o.OrderNo,
+                             o.CustomerID,
+                             o.PMethod,
+                             o.GTotal
+                         }).FirstOrDefault();
+
+            var orderDetails = (from oi in db.OrderItems
+                                join i in db.Items on oi.ItemID equals i.ItemID
+                                where oi.OrderID == id
+                                select new
+                                {
+                                    oi.OrderID,
+                                    oi.OrderItemID,
+                                    oi.ItemID,
+                                    ItemName = i.Name,
+                                    i.Price,
+                                    oi.Quantity,
+                                    Total = oi.Quantity * i.Price
+                                }).ToList();
+           
+            return Ok(new { order, orderDetails });
+        }
+
+        // PUT: api/Order/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrder(long id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.OrderID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Order
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult PostOrder(Order order)
+        {
+            try
+            {
+                //----save data Order table:Master
+                db.Orders.Add(order);
+                db.SaveChanges();
+                //----save data OrderItems table:Detail
+                //foreach (var item in order.OrderItems)
+                //{
+                //    db.OrderItems.Add(item); 
+                //}
+                //db.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // DELETE: api/Order/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult DeleteOrder(long id)
+        {
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            db.Orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(long id)
+        {
+            return db.Orders.Count(e => e.OrderID == id) > 0;
+        }
+    }
+}
